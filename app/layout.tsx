742ef857834117ca67208a9644914d05584b3071
Abhigael Mendez Carranza <62267297+AbhigaelCarranza@users.noreply.github.com>
2024-07-21 19:44:45 +0000

    change on metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,18 @@ import Header from '@/components/header';
 
 export const metadata = {
   title: "Boda Zaira & Abhigael",
-  description:
-    "Te invitamos a nuestra Boda Zaira & Abhigael",
-  metadataBase: new URL("https://bodazairaabhigael.com"),
+  description: "Te invitamos a nuestra Boda Zaira & Abhigael",
+  metadataBase: new URL("https://boda-zaira-abhigael.vercel.app"),
+  openGraph: {
+    images: [
+      {
+        url: "https://boda-zaira-abhigael.vercel.app/_next/image?url=%2Fboda%2FIMG_2181.jpg&w=828&q=75",
+        width: 800,
+        height: 600,
+        alt: "Boda Zaira & Abhigael",
+      },
+    ],
+  },
 };
 
 export default async function RootLayout({
@@ -21,6 +30,9 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:image" content="https://boda-zaira-abhigael.vercel.app/_next/image?url=%2Fboda%2FIMG_2181.jpg&w=828&q=75" />
+      </head>
       <body className={cx(sfPro.variable, inter.variable)}>
         <div className="fixed h-screen w-full bg-fondo" />
         <Suspense fallback="...">
